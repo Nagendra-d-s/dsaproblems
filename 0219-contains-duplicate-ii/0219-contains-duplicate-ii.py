aaf7a21c50bb29f866a1a175e646cadabd1eaec7
Nagendra D S <nagendrads1428@gmail.com>
2026-08-14 18:52:56 +0530

    Time: 51 ms (38.36%), Space: 25.8 MB (23.44%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bo"""
+        d={}
+        for i in range(len(nums)):
+            if nums[i] in d and abs(i-d[nums[i]])<=k:
+                return True
+            d[nums[i]]=i
+        return False
